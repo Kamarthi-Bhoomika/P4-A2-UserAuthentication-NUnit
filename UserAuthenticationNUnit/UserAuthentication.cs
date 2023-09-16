e180a1a016f77e5e67877cfbdb45dadc009f20f1
Kamarthi-Bhoomika <kbhoomika05@gmail.com>
2023-09-16 13:55:12 +0530

    NUnit Initial commit

---
+++ UserAuthenticationNUnit/UserAuthentication.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAuthenticationNUnit
+{
+    public class UserAuthentication
+    {
+        private List<User> users = new List<User>();
+        public void UserRegistration(string username, string password)
+        {
+            users.Add(new User { Username = username, Password = password });
+        }
+        public bool UserLogin(string username, string password)
+        {
+            return users.Exists(u => u.Username == username && u.Password == password);
+        }
+        public void PasswordReset(string username, string newPassword)
+        {
+            var user = users.Find(u => u.Username == username);
+            if (user != null)
+            {
+                user.Password = newPassword;
+            }
+        }
+    }
+}
---
+++ UserAuthenticationNUnit/UserAuthenticationTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAuthenticationNUnit
+{
+    [TestFixture]
+    public class UserAuthenticationTests
+    {
+        private UserAuthentication userAuthentication;
+        [SetUp]
+        public void Setup()
+        {
+            userAuthentication = new UserAuthentication();
+        }
+        [Test]
+        public void TestUserRegistration()
+        {
+            userAuthentication.UserRegistration("sam", "sam@123");
+            Assert.IsTrue(userAuthentication.UserLogin("sam", "sam@123"));
+        }
+        [Test]
+        public void TestUserLogin()
+        {
+            userAuthentication.UserRegistration("sam", "sam@123");
+            Assert.IsTrue(userAuthentication.UserLogin("sam", "sam@123"));
+            Assert.IsFalse(userAuthentication.UserLogin("sam", "wrongpwd"));
+            Assert.IsFalse(userAuthentication.UserLogin("user1", "pwd"));
+        }
+        [Test]
+        public void TestPasswordReset()
+        {
+            userAuthentication.UserRegistration("sam", "sam@123");
+            userAuthentication.PasswordReset("sam", "newpwd");
+            Assert.IsTrue(userAuthentication.UserLogin("sam", "newpwd"));        }
+    }
+}
